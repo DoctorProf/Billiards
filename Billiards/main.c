c8f3dd99e2604f2ca17a56c2957963bd2293eec3
Dmitrii Osipov <dimaosipov907@gmail.com>
2024-11-16 12:59:00 +0300

    unluck

--- Billiards/main.c
+++ Billiards/main.c
@@ -2,53 +2,105 @@
 #include <stdlib.h>
 #include <math.h>
 #include "Headers/Ball.h"
+#include "Headers/Cell.h"
+#include <time.h>
 
-int main()
+double distance(Vector2 pos_ball_1, Vector2 pos_ball_2)
 {
-	const float damping = 0.1;
-	const int screen_width = 800;
-	const int screen_height = 400;
-	const int fps = 120;
-	int radius = 10;
-	int holes_positions[6][2] = { {radius, radius},
-								  {400 + radius, radius},
-								  {800 - radius, radius},
-								  {radius, 400 - radius},
-								  {400, 400 - radius},
-								  {800 - radius, 400 - radius}
-	};
-	const int size = 16;
-	Ball* balls = (Ball*)malloc(size * sizeof(Ball));
-
+	double x = pos_ball_2.x - pos_ball_1.x;
+	double y = pos_ball_2.y - pos_ball_1.y;
+	return sqrt(pow(x, 2) + pow(y, 2));
+}
+int generateBalls(Ball* balls, int screen_width, int screen_height, int radius)
+{
+	int balls_count = 0;
 	float start_x = round(screen_width / 1.5);
 	float start_y = screen_height / 2;
-	int balls_count = 0;
+	int j = 0;
 	for (int i = 0; i < 5; ++i)
 	{
-		start_y -= radius;
-		start_x += 2 * radius;
 		for (int j = 0; j <= i; ++j)
 		{
-			Ball ball;
-			ball.position.x = start_x;
-			ball.position.y = start_y + 2 * (radius + 1) * j;
-			ball.velocity.x = 0;
-			ball.velocity.y = 0;
-			ball.type = Base;
-			balls[balls_count] = ball;
+			balls[balls_count].position = (Vector2){ start_x , start_y + 2 * (radius + 1) * j };
+			balls[balls_count].velocity = (Vector2){ 0 ,0 };
+			balls[balls_count].type = Base;
 			++balls_count;
 		}
+		start_y -= radius;
+		start_x += 2 * radius;
 	}
-	Ball play_ball;
-	play_ball.position.x = (float)round(screen_width / 2.5);
-	play_ball.position.y = screen_height / 2;
-	play_ball.velocity.x = 0;
-	play_ball.velocity.y = 0;
-	play_ball.type = Main;
-	balls[balls_count] = play_ball;
+	return balls_count;
+}
+void generateGrid(int width, int height, Cell*** grid)
+{
+	for (int i = 0; i < height; ++i)
+	{
+		grid[i] = (Cell*)malloc(width * sizeof(Cell));
+		for (int j = 0; j < width; ++j)
+		{
+			grid[i][j]->balls_index = NULL;
+		}
+	}
+}
+
+void checkCells(Cell*** grid, int width, int height, Ball* balls, int balls_count, int radius)
+{
+	for (int i = 0; i < balls_count; ++i)
+	{
+		int x = balls[i].position.x / (2 * radius);
+		int y = balls[i].position.y / (2 * radius);
+		if (x >= 0 && x < width && y >= 0 && y < height)
+		{
+			(*grid)[y][x].balls_index = (int*)malloc(sizeof(int));
+			(*grid)[y][x].balls_index = i;
+		}
+	}
+}
+void collisionBalls(Ball* balls, int ball_count)
+{
+}
+int main()
+{
+	float damping = 0.1;
+	int screen_width = 800;
+	int screen_height = 400;
+	int radius = 10;
+
+	int holes_positions[6][2] =
+	{
+		{radius, radius},
+		{screen_width / 2 + radius, radius},
+		{screen_width - radius, radius},
+		{radius, screen_height - radius},
+		{screen_width / 2, screen_height - radius},
+		{screen_width - radius, screen_height - radius}
+	};
+
+	int size = 16;
+	Ball* balls = malloc(size * sizeof(Ball));
+
+	int width_grid = screen_width / (2 * radius);
+	int height_grid = screen_height / (2 * radius);
+
+	Cell** grid = (Cell**)malloc(height_grid * sizeof(Cell*));
+	if (grid == NULL)
+	{
+		fprintf(stderr, "Ошибка выделения памяти\n");
+		free(balls);
+		return 1;
+	}
+	int balls_count = generateBalls(balls, screen_width, screen_height, radius);
+
+	balls[balls_count].position = (Vector2){ (float)round(screen_width / 2.5) , screen_height / 2 };
+	balls[balls_count].velocity = (Vector2){ 0 ,0 };
+	balls[balls_count].type = Main;
+	Ball* main_ball = &balls[balls_count];
 	++balls_count;
+	generateGrid(width_grid, height_grid, &grid);
+	checkCells(&grid, width_grid, height_grid, balls, balls_count, radius);
+
 	InitWindow(screen_width, screen_height, "Billiards");
-	SetTargetFPS(fps);
+	SetTargetFPS(120);
 
 	while (!WindowShouldClose())
 	{
@@ -63,10 +115,23 @@ int main()
 		{
 			Ball ball = balls[i];
 			DrawCircle(ball.position.x, ball.position.y, radius, ball.type != Main ? WHITE : RED);
+			DrawCircleLines(ball.position.x, ball.position.y, radius, BLACK);
 		}
 		EndDrawing();
 	}
 	CloseWindow();
 	free(balls);
+	if (*grid != NULL)
+	{
+		for (int i = 0; i < height_grid; ++i)
+		{
+			for (int j = 0; j < width_grid; ++j)
+			{
+				free(grid[i][j].balls_index);
+			}
+			free(grid[i]);
+		}
+		free(*grid);
+	}
 	return 0;
 }
